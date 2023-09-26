f7443b5439aba09812f01f88d71d41175c520dac
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-09-26 19:09:40 +0400

    fix: schedule and group

--- apps/server/typings/contact.py
+++ apps/server/typings/contact.py
@@ -8,8 +8,8 @@ class ContactInput(BaseModel):
     phone: str
     email: Optional[str]
     description: Optional[str]
-    source_type: str #later enum (web-scrapping, notion, db, and so on)
     workspace_id: Optional[UUID4] 
+    group_id: str
 
 class ContactOutput(BaseModel):
     id: str
@@ -18,11 +18,10 @@ class ContactOutput(BaseModel):
     phone: str
     email: Optional[str]
     description: Optional[str]
-    status: str
+    status: Optional[str]
     workspace_id: Optional[UUID4]
     group_id: Optional[UUID4]
     is_deleted: bool
-    is_public: bool
     account_id: UUID4
     created_by: Optional[UUID4]
     modified_by: Optional[UUID4]
