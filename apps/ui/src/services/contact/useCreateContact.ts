d1360721821bdea8be85793299290cbbde5793d4
levanion <levanion1998@gmail.com>
2023-10-05 16:12:37 +0400

    feat: createContact service

---
+++ apps/ui/src/services/contact/useCreateContact.ts
@@ -0,0 +1,35 @@
+import { useMutation } from '@apollo/client'
+
+import CREATE_CONTACT_GQL from '../../gql/ai/call/contact/createContact.gql'
+
+interface CreateContactInput {
+  name?: string
+  agent_id?: string
+  team_id?: string
+}
+
+export const useCreateContactService = () => {
+  const [mutation] = useMutation(CREATE_CONTACT_GQL)
+
+  const createContactService = async (input: any) => {
+    // const { name, agent_id, team_id } = input
+
+    const {
+      data: { createContact },
+    } = await mutation({
+      variables: {
+        input: {
+          name: 'First test',
+          description: 'desc',
+          group_id: '',
+          email: 'Levanion1998@gmail.com',
+          phone: '592444502',
+        },
+      },
+    })
+
+    return createContact
+  }
+
+  return [createContactService]
+}
