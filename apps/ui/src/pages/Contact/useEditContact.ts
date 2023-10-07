de792c637758a1c0df81a57f52d440a1d345bf7f
levanion <levanion1998@gmail.com>
2023-10-07 11:56:22 +0400

    feat: contact by id service

--- apps/ui/src/pages/Contact/useEditContact.ts
+++ apps/ui/src/pages/Contact/useEditContact.ts
@@ -3,15 +3,67 @@ import { useFormik } from 'formik'
 
 import { useContext, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { useAgentByIdService } from 'services/agent/useAgentByIdService'
-import { useUpdateAgentService } from 'services/agent/useUpdateAgentService'
-import { agentValidationSchema } from 'utils/validationsSchema'
+
+import { useContactByIdService } from 'services/contact/useContactByIdService'
 
 export const useEditContact = () => {
+  const { setToast } = useContext(ToastContext)
+
   const navigate = useNavigate()
   const params = useParams()
 
   const { contactId } = params
 
-  return {}
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { data: contactById } = useContactByIdService({ id: contactId || '' })
+
+  const defaultValues = {
+    contact_name: contactById?.name,
+    contact_description: contactById?.description,
+    contact_group_id: contactById?.group_id,
+    contact_email: contactById?.email,
+    contact_phone: contactById?.phone,
+  }
+
+  const handleSubmit = async (values: any) => {
+    setIsLoading(true)
+    try {
+      const contactInput = {
+        name: values.contact_name,
+        description: values.contact_description,
+        group_id: values.contact_group_id,
+        email: values.contact_email,
+        phone: values.contact_phone,
+      }
+
+      // await createContactService(contactInput)
+
+      // await refetchContacts()
+      setToast({
+        message: 'New Contact was Created!',
+        type: 'positive',
+        open: true,
+      })
+      navigate('/contacts')
+    } catch (e) {
+      setToast({
+        message: 'Failed To Add Contact!',
+        type: 'negative',
+        open: true,
+      })
+    }
+    setIsLoading(false)
+  }
+  const formik = useFormik({
+    initialValues: defaultValues,
+    enableReinitialize: true,
+
+    onSubmit: async values => handleSubmit(values),
+  })
+
+  return {
+    formik,
+    isLoading,
+  }
 }
